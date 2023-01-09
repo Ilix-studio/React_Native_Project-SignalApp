0e225cf073eef3eb33f4fc903829f23e56924d50
Ilix-studio <ilishjyoti17@gmail.com>
2023-01-09 19:25:20 +0530

    inital just setup i.e incomp

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAQfc3EDUyJoFa0w2AfSfZKHwmGB4t3WBw",
+    authDomain: "signal-clone-9228d.firebaseapp.com",
+    projectId: "signal-clone-9228d",
+    storageBucket: "signal-clone-9228d.appspot.com",
+    messagingSenderId: "136948200446",
+    appId: "1:136948200446:web:ad6e080ead57d89f0e06af"
+  };
